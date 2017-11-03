c164987e25872c881bfa15494562f0f1c3d8673d
Michael Brown <mbrown488@gmail.com>
2017-11-03 15:18:19 -0700

    Randomizer in place, storage of onclick needed

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 //Create an array to house pictures:
 var marketProjectPics = ['img/bag.jpg','img/banana.jpg','img/bathroom.jpg','img/boots.jpg','img/breakfast.jpg','img/bubblegum.jpg','img/chair.jpg','img/cthulhu.jpg','img/dog-duck.jpg','img/dragon.jpg','img/pen.jpg','img/pet-sweep.jpg','img/scissors.jpg','img/shark.jpg','img/sweep.png','img/tauntaun.jpg','img/unicorn.jpg','img/usb.gif','img/water-can.jpg','img/wine-glass.jpg']
 console.log(marketProjectPics);
-var PicObjects = [];
+var picObjects = [];
+var randArr = [];
 
 //create pic objects TestMarketPics(name,location)
 function TestMarketPics (name,picLocation){
   this.name = name;
   this.picLocation = picLocation;
-  PicObjects.push(this);
+  picObjects.push(this);
   this.totalSelections = 0;
   this.totalViews = 0;
 }
@@ -35,28 +36,82 @@ new TestMarketPics ('usb','img/usb.gif');
 new TestMarketPics ('water-can','img/water-can.jpg');
 new TestMarketPics ('wine-glass','img/wine-glass.jpg');
 
-//Randomizer that cycles throught the pictures
-function picRandom(){
-  return Math.floor(Math.random() * TestMarketPics[1]);
-};
-console.log(picRandom());
-
-function locRandom(){
-  return Math.floor(Math.random() * TestMarketPics[1]);
-}
-console.log(locRandom());
-
-var randPicSection = document.getElementById('randPicSection');
 var leftPic = document.getElementById('leftPic');
-leftPic.setAttribute('src',leftPic);
 var centerPic = document.getElementById('centerPic');
-centerPic.setAttribute('src',centerPic );
 var rightPic = document.getElementById('rightPic');
-rightPic.setAttribute('src',rightPic);
 
-function renderPics(){
-  leftPic = picRandom;
-  rightPic = picRandom;
-  centerPic = picRandom;
-}
-renderPics();
+leftPic.addEventListener('click',picRandom);
+rightPic.addEventListener('click',picRandom2);
+centerPic.addEventListener('click',picRandom3);
+
+function picRandom(){
+  var randomizer = Math.floor(Math.random() * marketProjectPics.length);
+  leftPic.src = picObjects[randomizer].picLocation;
+};
+picRandom();
+
+function picRandom2(){
+  var randomizer = Math.floor(Math.random() * marketProjectPics.length);
+  centerPic.src = picObjects[randomizer].picLocation;
+};
+picRandom2();
+
+function picRandom3(){
+  var randomizer = Math.floor(Math.random() * marketProjectPics.length);
+  rightPic.src = picObjects[randomizer].picLocation;
+};
+picRandom3();
+
+// var leftPic = document.getElementById('leftPic');
+// var centerPic = document.getElementById('centerPic');
+// var rightPic = document.getElementById('rightPic');
+//
+//
+// var leftImg;
+// var centerImg;
+// var rightImg;
+//
+// function render() {
+//   var randomPic = function() {
+//     for(var i = 0; i< picObjects.length;i++){
+//       leftimg = picRandom()[i];
+//       centerImg = picRandom()[i];
+//       rightImg = picRandom()[i];
+//       randArr.push(leftimg,centerImg,rightImg);
+//       leftPic.setAttribute('scr',leftimg.picLocation);
+//       centerPic.setAttribute('src',centerImg.picLocation);
+//       rightPic.setAttribute('src',rightImg.picLocation);
+//     }
+//     randomPic()
+//   }
+// }
+// render()
+// // function renderPic(){
+// //   for(var i = 0; i < TestMarketPics.length;i++){
+// // var randPicSection = document.getElementById('randPicSection');
+// // var leftPic = document.getElementById('leftPic');
+// // leftPic.setAttribute('src',leftPic.picLocation[i]);
+// // var centerPic = document.getElementById('centerPic');
+// // centerPic.setAttribute('src',centerPic.picLocation[i]);
+// // var rightPic = document.getElementById('rightPic');
+// // rightPic.setAttribute('src',rightPic.picLocation[i]);
+// // }
+// // }
+// // renderPic;
+//
+//
+// // Randomizer that cycles throught the pictures
+//
+// //
+// // function locRandom(){
+// //   return Math.floor(Math.random() * TestMarketPics[1]);
+// // }
+//
+//
+//
+// // function renderPics(){
+// //   leftPic = picRandom;
+// //   rightPic = picRandom;
+// //   centerPic = picRandom;
+// // }
+// // renderPics();
